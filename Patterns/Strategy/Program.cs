6f28633053f8c4a653129e961e8e45eb982e3570
Fkyeah <dimx63@mail.ru>
2022-02-06 16:52:13 +0300

    Демонстрация паттерна

---
+++ Patterns/Strategy/Program.cs
@@ -0,0 +1,53 @@
+﻿using Strategy.Ducks;
+using Strategy.VariableBehavior.Fly;
+using System;
+using System.Collections.Generic;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>
+            {
+                new WildDuck(),
+                new HomeDuck(),
+                new RubberDuck(),
+                new WoodenDuck(),
+            };
+
+            Console.WriteLine("Крякаем");
+            foreach (var duck in ducks)
+            {
+                duck.Display();
+                duck.PerformQuack();
+            }
+
+            Console.WriteLine("Плаваем");
+            foreach (var duck in ducks)
+            {
+                duck.Display();
+                duck.Swim();
+            }
+
+            Console.WriteLine("Летаем");
+            foreach (var duck in ducks)
+            {
+                duck.Display();
+                duck.PerfromFly();
+            }
+
+            Console.WriteLine("Изменяем поведение во время выполнения программы");
+            Duck modelDuck = new ModelDuck();
+            modelDuck.Display();
+            modelDuck.PerfromFly();
+
+            modelDuck.FlyBehavior = new FlyRocketPowered();
+            modelDuck.Display();
+            modelDuck.PerfromFly();
+
+            Console.ReadLine();
+        }
+    }
+}
