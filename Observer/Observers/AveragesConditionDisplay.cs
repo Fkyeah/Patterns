cd0bb2a19a5c673c60432fdffa1ff13ce28c62cc
Fkyeah <dimx63@mail.ru>
2022-02-06 19:55:26 +0300

    Настройка наблюдателей

---
+++ Observer/Observers/AveragesConditionDisplay.cs
@@ -0,0 +1,43 @@
+﻿using Observer.Interfaces;
+using System;
+
+namespace Observer.Observers
+{
+    public class AveragesConditionDisplay : IObserver, IDisplayElement
+    {
+        public AveragesConditionDisplay(ISubject weatherData)
+        {
+            _weatherData = weatherData;
+            _weatherData.RegisterObserver(this);
+        }
+
+        private ISubject _weatherData;
+        private float _maxTemperature;
+        private float _avgTemperature;
+        private float _minTemperature;
+
+        public void Display()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nAveragesConditionDisplay:");
+            Console.WriteLine("Max temperature - {0}", _maxTemperature);
+            Console.WriteLine("Average temperature - {0}", _avgTemperature);
+            Console.WriteLine("Min temperature - {0}", _minTemperature);
+        }
+
+        public void Update(float temperature, float humidity, float pressure)
+        {
+            if (_minTemperature == 0)
+                _minTemperature = temperature;
+
+            if (temperature > _maxTemperature)
+                _maxTemperature = temperature;
+            else if (temperature < _minTemperature)
+                _minTemperature = temperature;
+
+            _avgTemperature = (_maxTemperature + _minTemperature) / 2;
+
+            Display();
+        }
+    }
+}
---
+++ Observer/Observers/CurrentConditionDisplay.cs
@@ -0,0 +1,33 @@
+﻿using Observer.Interfaces;
+using System;
+
+namespace Observer.Observers
+{
+    public class CurrentConditionDisplay : IObserver, IDisplayElement
+    {
+        public CurrentConditionDisplay(ISubject weatherData)
+        {
+            _weatherData = weatherData;
+            _weatherData.RegisterObserver(this);
+        }
+
+        private float _temperature;
+        private float _humidity;
+        private ISubject _weatherData;
+
+        public void Display()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nCurrentConditionDisplay:");
+            var message = string.Format("Current conditions: {0}F degrees and {1}% humidity", _temperature, _humidity);
+            Console.WriteLine(message);
+        }
+
+        public void Update(float temperature, float humidity, float pressure)
+        {
+            _temperature = temperature;
+            _humidity = humidity;
+            Display();
+        }
+    }
+}
