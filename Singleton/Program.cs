d464d0864b4de4502e2d761fdc8de9210ed23652
Fkyeah <dimx63@mail.ru>
2022-02-23 13:53:19 +0300

    Паттерн одиночка

---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstInstance = Singleton.GetInstance();
+            Console.WriteLine(firstInstance.Description);
+
+            var secondInstance = Singleton.GetInstance();
+            Console.WriteLine(secondInstance.Description);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton
+{
+    public class Singleton
+    {
+        private Singleton()
+        {
+            Description = string.Format("Number of this instance - {0}", ++_instanceNumber);
+        }
+
+        private int _instanceNumber = 0;
+        private static Singleton _instance;
+        private static object _locker = new object();
+        public string Description { get; private set; }
+
+        public static Singleton GetInstance()
+        {
+            lock(_locker)
+            {
+                if (_instance is null)
+                    _instance = new Singleton();
+
+                return _instance;
+            }
+        }
+    }
+}
